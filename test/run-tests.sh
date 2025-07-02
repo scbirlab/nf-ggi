2ebd54fd1a3fb78d4f55da0bf7a2b84e9e8a8a99
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-07-02 13:10:24 +0100

    Change test order

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -26,6 +26,17 @@ script_dir="$(dirname $0)"
 nextflow run "$script_dir"/.. \
     -resume $docker_flag \
     --test \
+    --outputs bait-taxon-test \
+    --organism_id 1773 \
+    --bait 28369 \
+    --bait_is_taxon \
+    --interspecies \
+    --dca --rf2t \
+    --bfd "$bfd" --uniclust "$uniclust"
+    
+nextflow run "$script_dir"/.. \
+    -resume $docker_flag \
+    --test \
     --outputs custom-test \
     --organism_id 559292 \
     --filename "$script_dir"/custom/inputs/sgadata_costanzo2009_rawdata_101120-wheader_10q.txt \
@@ -41,17 +52,6 @@ nextflow run "$script_dir"/.. \
     --outputs bait-test \
     --organism_id 559292 \
     --bait P00931 \
-    --dca --rf2t \
-    --bfd "$bfd" --uniclust "$uniclust"
-
-nextflow run "$script_dir"/.. \
-    -resume $docker_flag \
-    --test \
-    --outputs bait-taxon-test \
-    --organism_id 1773 \
-    --bait 28369 \
-    --bait_is_taxon \
-    --interspecies \
     --dca --rf2t \
     --bfd "$bfd" --uniclust "$uniclust"
 
